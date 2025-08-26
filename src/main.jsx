dabe7559ca847fae0cf2ac4eeb850aea57011358
devillaa <devillajoaopedro532@gmail.com>
2025-08-26 11:20:52 -0300

    primeiro commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Paragrafo from './Paragrafo.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+
+    <Paragrafo />
+    <Paragrafo />
+    <Paragrafo />
+    <Paragrafo />
+    
+  </StrictMode>,
+)

102ce587d74322041ed376df23ab41da1775a849
devillaa <devillajoaopedro532@gmail.com>
2025-09-16 11:21:56 -0300

    fetch useEffect api and blabla
    json

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,36 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/home'
 import Sobre from './pages/Sobre'
 import './App.css'
+import './tema.css'
 import AppTarefas from './pages/Tarefas/AppTarefas'
-import { ThemeProvider } from './contexts/ThemeContext'
+import { ThemeContext, ThemeProvider } from './contexts/ThemeContext'
 import ThemeToggleButton from './components/ThemeToggleButton'
+import { useContext } from 'react'
+import Usuarios from './pages/usuarios'
 
 function App() {
 
   return (
     <ThemeProvider>
       <BrowserRouter>
-        <nav>
-          <ul>
-            <li> <Link to="/">Home</Link> </li>
-            <li> <Link to="/sobre">Sobre</Link> </li>
-            <li> <Link to="/tarefas" > Tarefas </Link> </li>
-          </ul>
+        <div className='escuro'>
+          <nav>
+            <ul>
+              <li> <Link to="/">Home</Link> </li>
+              <li> <Link to="/sobre">Sobre</Link> </li>
+              <li> <Link to="/tarefas" > Tarefas </Link> </li>
+              <li> <Link to="/usuarios" > Usuários </Link> </li>
+            </ul>
 
-          <ThemeToggleButton></ThemeToggleButton>
-        </nav>
-        
+            <ThemeToggleButton></ThemeToggleButton>
+          </nav>
+        </div>
+
         {/* Define as rotas */}
         <div>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="usuarios"  element={ <Usuarios/> } /> 
             <Route path="/sobre" element={<Sobre />} />
             <Route path="/tarefas" element={ <AppTarefas/> } />
           </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     const {theme} = useContext(ThemeContext);
     
     return (
-        <div>
+        <div className={theme}>
             <h2>Página Inicial {theme} </h2>
             <p>Bem-vindos à página inicial desta aplicação gloriosa!</p>
         </div>
